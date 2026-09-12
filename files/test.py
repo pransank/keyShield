d5f370cee9b9df2a64838ff16b31b9d13686ff04
pransank <pranavsankar614@gmail.com>
2026-09-12 09:43:04 -0700

    Created version 1 to remove senstive data such as API keys using basic regex checks on Python

---
+++ files/test.py
@@ -0,0 +1,6 @@
+import requests
+
+api_key = "sk-123456789"
+username = "pranav"
+
+print(api_key)
---
+++ files/test_secured/test_secured.py
@@ -0,0 +1,8 @@
+import os
+
+import requests
+
+api_key = os.getenv("API_KEY")
+username = "pranav"
+
+print(api_key)
